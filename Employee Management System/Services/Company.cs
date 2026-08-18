d1031549c6ce2b2e946dfac9151ded7bce5e1a58
Mona Mohamed <monamo20006@gmail.com>
2026-08-18 17:50:25 +0300

    Added company collections

--- Employee Management System/Services/Company.cs
+++ Employee Management System/Services/Company.cs
@@ -6,6 +6,10 @@ namespace Employee_Management_System.Services
 {
     public class Company
     {
-
+        List<string> ActiveEmployees = new List<string>();
+        Dictionary<int,string> Departments = new Dictionary<int,string>();
+        Queue<string> OnBoarding = new Queue<string>();
+        Stack<string> ActionHistory = new Stack<string>();
+        HashSet<string> UniqueSkills = new HashSet<string>();
     }
 }
